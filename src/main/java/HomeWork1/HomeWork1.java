6e50adbe3cf5cea370c959c7b30d37202abedac9
wolfchkov <wolfchkovaj@gmail.com>
2018-02-09 22:01:08 +0200

    Update HomeWork1.java
    мелкие замечания

--- src/main/java/HomeWork1/HomeWork1.java
+++ src/main/java/HomeWork1/HomeWork1.java
@@ -14,24 +14,29 @@ public class HomeWork1 {
     public static long ur1() {
         long  x=10, y = 200, z = 2, n = 50;
         long k = x + (y - 100) * z / n;
+        //если printf, то не нужно использовать конкатенацию
+        //System.out.printf("1) x + (y-100) * z / n = %f %n", k);
         System.out.printf("1) x + (y-100) * z / n= " + k +"%n");
         return 0;
     }
     public static int ur2() {
         int x=100, y=-20, z=2, n=5;
         int k=(x-(50+y*z)*n)+x;
+        //если printf, то не нужно использовать конкатенацию
         System.out.printf("2) (x - (50+y*z) *n) +x= "+k+"%n");
         return 0;
     }
        public static double ur3() {
         float x=10, y=27, z=2, n=6001;
         float k=x* (y / (z-1) +555) -n;
+        //если printf, то не нужно использовать конкатенацию
         System.out.printf("3) x* (y / (z-1) +555) -n= "+k+"%n");
         return 0;
     }
        public static double ur4() {
         float x=100, y=27, z=2, n=5;
         float k=-x/y+z*(n+1);
+        //если printf, то не нужно использовать конкатенацию
         System.out.printf("4) -x/y +z * (n+1)= "+k+"%n");
         return 0;
     }
@@ -53,7 +58,7 @@ public class HomeWork1 {
         int t3=x/100%10;
         int t2=x/1000%10;
         int t1=x/10000%10;
-
+        //если printf, то не нужно использовать конкатенацию
         System.out.printf(t5+"%n"+t4+"%n"+t3+"%n"+t2+"%n"+t1);
         return 0;
     }
@@ -69,6 +74,7 @@ Math.sqrt(a);
         double X=3, Y=1, Z=-1;
 
     double sqrt=Math.sqrt((X*X)+(Y*Y)+(Z*Z));
+    //если printf, то не нужно использовать конкатенацию
     System.out.printf("Длина вектора (X, Y, Z) = "+sqrt);
 
     }
